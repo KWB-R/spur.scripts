f5b70c0b4ea5713acc72774d1141510ff5252aa3
RobertoTatisMuvdi <rtatmuv@KWB.BERLIN>
2020-11-11 09:18:29 +0100

    klaas' adjustments

--- preBaSaR_conc.R
+++ preBaSaR_conc.R
@@ -63,52 +63,76 @@ spur_pkw <- getMonitoringTable(
 
 
 basar_bbr_kanal <- getMonitoringTable(
-  
-)
+  subfolder = 'data_facade_vol_c_annual',
+  dbTable = NA,
+  dbName = 'BBRs_20200518_conc.txt',
+  format = 'txt',
+  project = 'basar',
+  detLimOperator = 0.5,
+  site = 'bbr',
+  source = 'storm_sewer',
+  dateTimeFormat = '%Y-%m-%d %H:%M:%S', 
+  tz = 'Etc/GMT-1')
+
 
 basar_bbw_kanal <- getMonitoringTable(
-  
-)
+  subfolder = 'data_facade_vol_c_annual',
+  dbTable = NA,
+  dbName = 'BBWs_20200518_conc.txt',
+  format = 'txt',
+  project = 'basar',
+  detLimOperator = 0.5,
+  site = 'bbw',
+  source = 'storm_sewer',
+  dateTimeFormat = '%Y-%m-%d %H:%M:%S', 
+  tz = 'Etc/GMT-1')
 
 
 # make tables for target substances
 Zn <- rbind(makeSubstanceTable(
   dbNames = c('basar_bbr_facade', 'basar_bbw_facade', 
               'basar_bbr_roof', 'basar_bbw_roof',
+              'basar_bbr_kanal','basar_bbw_kanal',
               'spur_pkw'),
   substance = 'Zn_total'),
   makeSubstanceTable(
     dbNames = c('basar_bbr_facade', 'basar_bbw_facade', 
                 'basar_bbr_roof', 'basar_bbw_roof',
+                'basar_bbr_kanal','basar_bbw_kanal',
                 'spur_pkw'),
     substance = 'Zn'))
 
 Cu <- rbind(makeSubstanceTable(
   dbNames = c('basar_bbr_facade', 'basar_bbw_facade', 
               'basar_bbr_roof', 'basar_bbw_roof',
+              'basar_bbr_kanal','basar_bbw_kanal',
               'spur_pkw'),
   substance = 'Cu_total'),
   makeSubstanceTable(
     dbNames = c('basar_bbr_facade', 'basar_bbw_facade', 
                 'basar_bbr_roof', 'basar_bbw_roof',
+                'basar_bbr_kanal','basar_bbw_kanal',
                 'spur_pkw'),
     substance = 'Cu'))
 
 Diuron <- makeSubstanceTable(
   dbNames = c('basar_bbr_facade', 'basar_bbw_facade', 
               'basar_bbr_roof', 'basar_bbw_roof',
+              'basar_bbr_kanal','basar_bbw_kanal',
               'spur_pkw'),
   substance = 'Diuron')
 
 Terbutryn <- makeSubstanceTable(
   dbNames = c('basar_bbr_facade', 'basar_bbw_facade', 
               'basar_bbr_roof', 'basar_bbw_roof',
+              'basar_bbr_kanal','basar_bbw_kanal',
               'spur_pkw'),
   substance = 'Terbutryn')
 
 Terbutryn_desethyl <- makeSubstanceTable(
   dbNames = c('basar_bbr_facade', 'basar_bbw_facade', 
               'basar_bbr_roof', 'basar_bbw_roof',
+              'basar_bbr_kanal','basar_bbw_kanal',
               'spur_pkw'),
   substance = 'Terbutryn_desethyl')
 
@@ -116,19 +140,29 @@ Terbutryn_2_hydroxy <- rbind(
   makeSubstanceTable(
     dbNames = c('basar_bbr_facade', 'basar_bbw_facade', 
                 'basar_bbr_roof', 'basar_bbw_roof',
+                'basar_bbr_kanal','basar_bbw_kanal',
                 'spur_pkw'),
     substance = 'Terbutryn.2.hydroxy'),
   makeSubstanceTable(
     dbNames = c('basar_bbr_facade', 'basar_bbw_facade', 
                 'basar_bbr_roof', 'basar_bbw_roof',
+                'basar_bbr_kanal','basar_bbw_kanal',
                 'spur_pkw'),
     substance = 'Terbutryn-2-hydroxy'))
 
 Mecoprop <- makeSubstanceTable(
   dbNames = c('basar_bbr_facade', 'basar_bbw_facade', 
               'basar_bbr_roof', 'basar_bbw_roof',
+              'basar_bbr_kanal','basar_bbw_kanal',
               'spur_pkw'), 
   substance = 'Mecoprop')
+
+Benzothiazol <- makeSubstanceTable(
+  dbNames = c('basar_bbr_facade', 'basar_bbw_facade', 
+              'basar_bbr_roof', 'basar_bbw_roof',
+              'basar_bbr_kanal','basar_bbw_kanal',
+              'spur_pkw'), 
+  substance = 'Benzothiazol')
 
 
 # statistics
@@ -145,6 +179,36 @@ plotSubstance(Terbutryn_desethyl)
 plotSubstance(Terbutryn_2_hydroxy)
 plotSubstance(Mecoprop)
 
+ALT <- read.table('data/Konz_ALT.csv', header = TRUE, sep = ';')
+NEU <- read.table('data/Konz_NEU.csv', header = TRUE, sep = ';')
+EFH <- read.table('data/Konz_NEU.csv', header = TRUE, sep = ';')
+GEW <- read.table('data/Konz_NEU.csv', header = TRUE, sep = ';')
+
+
+
+boxplot(concentration ~ source, data= Diuron, main = 'Diuron', xlab = NA , ylab = 'concentration [mg/L]')
+abline(h = (1000*c(ALT$Konz_Diuron, NEU$Konz_Diuron, EFH$Konz_Diuron, GEW$Konz_Diuron)), col= c('red', 'green','blue','orange' ), lwd = 1, lty = 2)
+legend(x= 3.4 , y=2900, legend = c('ALT', 'NEU', 'EFH', 'GEW'), col = c('red','green','blue','orange'), lty=2)
+
+boxplot(concentration ~ source, data= Mecoprop, main = 'Mecoprop', xlab = NA , ylab = 'concentration [mg/L]')
+abline(h = (1000*c(ALT$Konz_Mecoprop, NEU$Konz_Mecoprop, EFH$Konz_Mecoprop, GEW$Konz_Mecoprop)), col= c('red', 'green','blue','orange' ), lwd = 1, lty = 2)
+legend(x= 3.4 , y= 440, legend = c('ALT', 'NEU', 'EFH', 'GEW'), col = c('red','green','blue','orange'), lty=2)
+
+boxplot(concentration ~ source, data= Terbutryn, main = 'Terbutryn', xlab = NA , ylab = 'concentration [mg/L]')
+abline(h = (1000*c(ALT$Konz_Terbutryn, NEU$Konz_Terbutryn, EFH$Konz_Terbutryn, GEW$Konz_Terbutryn)), col= c('red', 'green','blue','orange' ), lwd = 1, lty = 2)
+legend(x= 3.4 , y=83, legend = c('ALT', 'NEU', 'EFH', 'GEW'), col = c('red','green','blue','orange'), lty=2)
+
+boxplot(concentration ~ source, data = Benzothiazol, main = 'Benzothiazol', xlab = NA , ylab = 'concentration [mg/L]')
+abline(h = (1000*c(ALT$Konz_Benzothiazol, NEU$Konz_Benzothiazol, EFH$Konz_Benzothiazol, GEW$Konz_Benzothiazol)), col= c('red', 'green','blue','orange' ), lwd = 1, lty = 2)
+legend(x= 1.22 , y=22, legend = c('ALT', 'NEU', 'EFH', 'GEW'), col = c('red','green','blue','orange'), lty=2)
+
+boxplot(concentration ~ source, data = Zn, main = 'Zn', xlab = NA , ylab = 'concentration [mg/L]')
+abline(h = (1000*c(ALT$Konz_Zn, NEU$Konz_Zn, EFH$Konz_Zn, GEW$Konz_Zn)), col= c('red', 'green','blue','orange' ),lwd = 1, lty = 2)
+legend(x= 3.4 , y=13000, legend = c('ALT', 'NEU', 'EFH', 'GEW'), col = c('red','green','blue','orange'), lty=2)
+
+boxplot(concentration ~ source, data = Cu, main = 'Cu', xlab = NA , ylab = 'concentration [mg/L]')
+abline(h = (1000*c(ALT$Konz_Cu, NEU$Konz_Cu, EFH$Konz_Cu, GEW$Konz_Cu)), col= c('red', 'green','blue','orange' ), lwd = 1, lty = 2)
+legend(x= 3.4 , y=790, legend = c('ALT', 'NEU', 'EFH', 'GEW'), col = c('red','green','blue','orange'), lty=2)
 
 
 
@@ -154,7 +218,7 @@ getMonitoringTable <- function(subfolder,
                                format, project, site, source,
                                detLimOperator, dateTimeFormat, 
                                tz){
-
+  
   library(dplyr)
   
   if(format == 'txt'){
@@ -217,17 +281,17 @@ getMonitoringTable <- function(subfolder,
   }
   
   db$concentration <- detLimManage(x = db$concentration, 
-                                      detLimOperator = detLimOperator)
-
+                                   detLimOperator = detLimOperator)
+  
   # in spur data, get rid of filter outlflow concentrations, and remove '_zu' in substance names
   if(project == 'spur'){
     index <- !grepl(pattern = '_ab$',
-                   x = db$substance)
+                    x = db$substance)
     db <- db[index, ]
     
     db$substance <- gsub(pattern = '_zu$', replacement = '', x = db$substance)
   }
-
+  
   # format dateTime columns
   tCols <- grep(pattern='tBeg|tEnd', x = colnames(db))
   for(col in tCols){
@@ -264,7 +328,7 @@ makeSubstanceTable <- function(dbNames, substance){
 }
 
 plotSubstance <- function(substance){
-
+  
   library(colorspace)
   
   nsites <- length(unique(substance$site))
