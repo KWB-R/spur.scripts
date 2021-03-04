9b7ca4df247bdb94c294817c592153a27eb91d12
klaaskenda <klaas.kenda@kompetenz-wasser.de>
2021-03-04 16:54:21 +0100

    got rid of unnecessary code

--- plotting_loads.R
+++ plotting_loads.R
@@ -4,9 +4,6 @@ substances <- c('Diuron', 'Mecoprop', 'Terbutryn', 'Benzothiazol', 'Zn', 'Cu')
 
 boxplot(simulated_loads, log = 'y',  ylab= 'loads [μg]', outline=FALSE, cex.axis= 1.1, cex.lab= 1.3, col = 3)
 
-max(simulated_loads[,index_substance])
-min(simulated_loads[,index_substance])
-
 for (substance in substances) {
   index_substance<- which(substances==substance)
   boxplot(simulated_loads[,index_substance], 
