73b212440c06d1a586fac3bee2169ee4c7c59123
klaaskenda <klaas.kenda@kompetenz-wasser.de>
2021-03-29 11:49:20 +0200

    hard code into loop

--- composition_catchment_area.R
+++ composition_catchment_area.R
@@ -1,3 +1,6 @@
+
+catchments <- c('Wuhle', 'Kleingewässer (Teiche, Tümpel, Gräben)', 'Großer Wannsee', 'Kleine Wannseekette', 'Groß-Glienicker See', 'Grunewaldseenkette', 'Tegeler See', 'Flughafensee', 'Schäfersee')
+
 ###load data
 # abimo runoff and OgRe information (roof, yard, street (last two include facade runoff))
 berlin_runoff <- foreign::read.dbf('data/berlin_runoff.dbf')
@@ -5,49 +8,26 @@ berlin_runoff <- setnames(berlin_runoff, old=c('runoff_str', 'runoff_yar', 'runo
 
 
 
-#Wuhle -> 884 BTF
-BTF_Wuhle <- subset(berlin_runoff, AGEB1=='Wuhle')
-#Fläche in m2
-AW <- sum(BTF_Wuhle$FLGES)
-W_ALT <- subset(BTF_Wuhle, OgRe_Type=='ALT')
-W_NEU <- subset(BTF_Wuhle, OgRe_Type=='NEU')
-W_EFH <- subset(BTF_Wuhle, OgRe_Type=='EFH')
-W_GEW <- subset(BTF_Wuhle, OgRe_Type=='GEW')
-W_STR <- subset(BTF_Wuhle, OgRe_Type=='STR')
-W_AND <- subset(BTF_Wuhle, OgRe_Type=='AND')
-
-xW_ALT<- sum(W_ALT$FLGES)/AW
-xW_NEu<- sum(W_NEU$FLGES)/AW
-xW_EFH<- sum(W_EFH$FLGES)/AW
-xW_GEW<- sum(W_GEW$FLGES)/AW
-xW_STR<- sum(W_STR$FLGES)/AW
-xW_AND<- sum(W_AND$FLGES)/AW
-
-Wuhle<- c(xW_ALT,xW_NEu,xW_EFH,xW_GEW,xW_STR,xW_AND)*100
-names(Wuhle)<- c('ALT','NEU','EFH','GEW','STR','AND')
-
-barplot(Wuhle, ylim = c(0,100), main = 'Wuhle')
-
-#kleine Seen -> 718 BTF
-BTF_kleine_Seen <- subset(berlin_runoff, AGEB1=='Kleingewässer (Teiche, Tümpel, Gräben)')
-#Fläche in m2
-AkS <- sum(BTF_kleine_Seen$FLGES)
-
-kS_ALT <- subset(BTF_kleine_Seen, OgRe_Type=='ALT')
-kS_NEU <- subset(BTF_kleine_Seen, OgRe_Type=='NEU')
-kS_EFH <- subset(BTF_kleine_Seen, OgRe_Type=='EFH')
-kS_GEW <- subset(BTF_kleine_Seen, OgRe_Type=='GEW')
-kS_STR <- subset(BTF_kleine_Seen, OgRe_Type=='STR')
-kS_AND <- subset(BTF_kleine_Seen, OgRe_Type=='AND')
-
-xkS_ALT<- sum(kS_ALT$FLGES)/AkS
-xkS_NEu<- sum(kS_NEU$FLGES)/AkS
-xkS_EFH<- sum(kS_EFH$FLGES)/AkS
-xkS_GEW<- sum(kS_GEW$FLGES)/AkS
-xkS_STR<- sum(kS_STR$FLGES)/AkS
-xkS_AND<- sum(kS_AND$FLGES)/AkS
-
-kleine_Seen<- c(xkS_ALT,xkS_NEu,xkS_EFH,xkS_GEW,xkS_STR,xkS_AND)*100
-names(kleine_Seen)<- c('ALT','NEU','EFH','GEW','STR','AND')
-
-barplot(kleine_Seen, ylim = c(0,100), main = 'kleine Seen')
+for(catchment in catchments){
+  
+  BTF_catchment <- subset(berlin_runoff, AGEB1 == catchment)
+  ROW <- sum(BTF_catchment$ROW)
+  ALT <- subset(BTF_catchment, OgRe_Type=='ALT')
+  NEU <- subset(BTF_catchment, OgRe_Type=='NEU')
+  EFH <- subset(BTF_catchment, OgRe_Type=='EFH')
+  GEW <- subset(BTF_catchment, OgRe_Type=='GEW')
+  STR <- subset(BTF_catchment, OgRe_Type=='STR')
+  AND <- subset(BTF_catchment, OgRe_Type=='AND')
+  
+  x_ALT <- sum(ALT$ROW)/ROW
+  x_NEU <- sum(NEU$ROW)/ROW
+  x_EFH <- sum(EFH$ROW)/ROW
+  x_GEW <- sum(GEW$ROW)/ROW
+  x_STR <- sum(STR$ROW)/ROW
+  x_AND <- sum(AND$ROW)/ROW
+  
+  composition <- 100* c( x_ALT, x_NEU, x_EFH, x_GEW, x_STR, x_AND) 
+  names(composition)<- c('ALT','NEU','EFH','GEW','STR','AND')
+  barplot(composition, ylim = c(0,100), main = catchment)
+  
+ }
