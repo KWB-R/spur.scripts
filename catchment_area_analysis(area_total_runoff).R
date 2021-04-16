3538d1f20c1768892bebef15220d034cd72d7c8b
klaaskenda <klaas.kenda@kompetenz-wasser.de>
2021-04-16 09:26:48 +0200

    kleines Script zum Vergleichen von Einzugsgebietsfläche und gesamten runoff eines Einzugsgebiets

---
+++ catchment_area_analysis(area_total_runoff).R
@@ -0,0 +1,17 @@
+###load data
+# abimo runoff and OgRe information (roof, yard, street (last two include facade runoff))
+berlin_runoff <- foreign::read.dbf('data/berlin_runoff.dbf')
+berlin_runoff <- setnames(berlin_runoff, old=c('runoff_str', 'runoff_yar', 'runoff_bit', 'runoff_zie', 'runoff_res', 'runoff_put','runoff_tot'), new= c('runoff_Strasse','runoff_Hof','runoff_Bitumendach','runoff_Ziegeldach','runoff_Dach_weitere','runoff_Putzfassade','runoff_total'))
+
+catchments <- c('Wuhle', 'Flughafensee')
+
+for(catchment in catchments){
+  
+  #selecting the surveyed catchemnt
+  current_catchment <- assign(paste0('BTF_', catchment), subset(berlin_runoff, AGEB1 == catchment))
+  #determine total runoff of the catchment
+  assign(paste0('total_runoff_', catchment), sum(current_catchment$runoff_total))
+  #determine total area of the catchment
+  assign(paste0('total_area_', catchment), sum(current_catchment$FLAECHE))
+  
+}
