db47ab0d2f3a02a98d7acc0fb4595d8510ebf4d0
klaaskenda <klaas.kenda@kompetenz-wasser.de>
2021-08-10 04:32:17 +0200

    Fokus auf nur drei Auswahlsubstanzen

--- Panke_quick_plot.R
+++ Panke_quick_plot.R
@@ -6,11 +6,12 @@ data.dir <- "data/"
 x_Panke <- read.table(file = file.path(data.dir, "Panke_wet_weather.csv"), sep = ";", dec = ".", header = TRUE, as.is = TRUE)
 
 #substances of interest
-substances <- c("zinc", "copper", "benzothiazole", "mecoprop", "diuron", "terbutryn")
+substances <- c('diuron', 'mecoprop', 'zinc') #c("diuron", "mecoprop", "terbutryn", "benzothiazole", "copper", "zinc" )
 
 #reduce Panke data to substances of interest
 index <- which(x_Panke$VariableName %in% substances)
 x_Panke <- x_Panke[index,]
 
 #simple boxplot
-boxplot(DataValue~VariableName, data = x_Panke, log = "y")
+#par(mgp=c(2.5, 1, 0), fin=c(5,4))
+boxplot(DataValue~VariableName, data = x_Panke, log = "y", las=1, col = 'lightblue', xlab='', ylab = 'µg/L', cex.lab=1.2, cex.axis=1.2 )
